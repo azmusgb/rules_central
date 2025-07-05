6f244a83485f112ef8d1599822e84ca5db94d368
azmusgb <36898133+azmusgb@users.noreply.github.com>
2025-07-05 19:03:18 -0400

    chore: tidy configuration and docs

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 """WSGI entry point for production servers."""
 
-from app import app
+from app import app as application
 
 if __name__ == "__main__":
     # Run with Flask's development server when executed directly.
-    app.run()
+    application.run()
