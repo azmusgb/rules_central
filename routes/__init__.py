bff1b162b1b999c9da7f656ea2e82872a6dd21dd
azmusgb <36898133+azmusgb@users.noreply.github.com>
2025-07-14 16:58:57 -0400

    Merge metrics API into analytics routes

--- routes/__init__.py
+++ routes/__init__.py
@@ -6,9 +6,6 @@ from .main import main
 from .collab import collab
 from .analytics_routes import analytics_routes
 from .user_routes import user_routes
-
-# Import metrics routes so they register with the API blueprint
-from . import metrics_api  # noqa: F401
 from all_routes import routes_bp
 
 # Register routes_bp first to avoid shadowing of /api/* endpoints
